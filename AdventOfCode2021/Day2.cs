e0b843069ed68177129662574de16de727c5ed0d
JK <11391313+bintzknocker@users.noreply.github.com>
2021-12-02 11:50:46 -0600

    day2

---
+++ AdventOfCode2021/Day2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    public class Day2
+    {
+        public Day2()
+        {
+
+        }
+
+        public void Go()
+        {
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Git\AdventOfCode2021\AdventOfCode2021\AdventOfCode2021\day2data.txt");
+            foreach(var line in lines)
+            {
+                var val = ParseLine(line);
+
+                if(line.StartsWith("forward"))
+                {
+                    horizontal += val;
+                    depth = depth + (aim * val);
+                }
+                else if(line.StartsWith("down"))
+                {
+                   // depth += val;
+                    aim += val;
+                }
+                else if (line.StartsWith("up"))
+                {
+                    //depth -= val;
+                    aim -= val;
+                }
+            }
+
+            Console.WriteLine(horizontal * depth);
+        }
+
+        private int ParseLine(string line)
+        {
+            return int.Parse(line.Replace("forward ", "").Replace("up ", "").Replace("down ", ""));
+        }
+    }
+}
---
+++ AdventOfCode2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AdventOfCode2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var d = new Day2();
+            d.Go();
+        }
+    }
+}
